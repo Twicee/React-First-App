92965006f4644fbba05d575f1653d9c960b0bf37
Adrian Hernandez <hernandeza257@gmail.com>
2024-01-18 02:46:51 -0800

    Part 13 small commit

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor"
-        } // the rest of the data
-      ]);
-    
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
 }
+return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form />
+    </div>
+  );
 
 export default MyApp;
